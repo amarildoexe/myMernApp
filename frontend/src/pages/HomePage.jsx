30e79f61fb0a9606392d88aacb96daf9a04081ed
amarildoexe <amarildoexe@users.noreply.github.com>
2026-01-05 23:08:52 +0100

    second update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -48,7 +48,11 @@ const HomePage = () => {
         {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
 
         {notes.length > 0 && !isRateLimited && (
+          
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="flex items-center justify-center">
+            <img src="/public/diddy.jpg" alt="diddy baby"  />
+            </div>
             {notes.map(note => (
               <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))} 
@@ -56,6 +60,7 @@ const HomePage = () => {
           </div>          
         )}
       </div>
+      
     </div>
   )
 };
